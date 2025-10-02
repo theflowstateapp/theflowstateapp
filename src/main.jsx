a341e12eb3fe861a406ff0136ab88c2e50f73158
transitionmarketingai <abhishekjohn@transitionmarketingai.com>
2025-10-02 16:44:39 +0530

    🔥 NUCLEAR: Clean AI-only deployment with vercelignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 )
